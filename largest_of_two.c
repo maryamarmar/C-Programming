228d20a7f30c32445481f944acd52ddbae0e18a8
Maryam Armar <maryamarmar28@gmail.com>
2024-12-27 13:15:34 +0530

    Add files via upload

---
+++ largest_of_two.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int A, B;
+printf("Enter values of A & B:");
+scanf("%d %d", &A, &B);
+if(A>B)
+printf("A is larger \n");
+else
+printf("B is larger \n");
+return 0;
+}
