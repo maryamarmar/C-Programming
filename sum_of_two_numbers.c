48c50fea9cb14fb5a216ccac5991f366332fa363
Maryam Armar <maryamarmar28@gmail.com>
2024-12-27 15:38:04 +0530

    Add files via upload

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int a, b, c;
+printf("Enter value of a:");
+scanf("%d", &a);
+printf("Enter value of b:");
+scanf("%d", &b);
+c = a+b;
+printf("The sum of given two numbers is %d. \n", c);
+return 0;
+}
